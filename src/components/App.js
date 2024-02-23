8567a314739c1b57f3327ff608c04416eff038ca
suryarajrs <suryars@outlook.in>
2024-02-23 19:14:21 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+
+  const [counter, setCounter]=useState(1)
 
   
   return (
     <div id="main">
+      <button onClick={()=>{setCounter((prev)=>prev+1)}} id='increment'>Increment</button>
+      <div  className={(counter%5==0 && counter%3==0)?("fizzbuzz"):(counter%3==0)?("fizz"):(counter%5==0)?("buzz"):("normal") } id="counter">{counter}</div>
+      <button onClick={()=>{setCounter((prev)=>prev-1)}} id='decrement'>Decrement</button>
 
     </div>
   )
